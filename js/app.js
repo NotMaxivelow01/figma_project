8b8b045254e4b9408f008353e2b2c09901f97137
NotMaxivelow01 <mfont@endenschool.fr>
2023-01-13 15:01:39 +0100

    git

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
-const img = document.getElementById('carousel');
-const rightBtn = document.getElementById('right-btn');
-const leftBtn = document.getElementById('left-btn');
-let pictures = ['image-slide-1.jpg', 'image-slide-2.jpg', "image-slide-3.jpg", "image-slide-4.jpg", "image-slide-5.jpg"];
-img.src = pictures[0];
-let position = 0;
-const moveRight = () => {
-    if (position >= pictures.length - 1) {
-        position = 0
-        img.src = pictures[position];
-        return;
-    }
-    img.src = pictures[position + 1];
-    position++;
-}
-const moveLeft = () => {
-    if (position < 1) {
-        position = pictures.length - 1;
-        img.src = pictures[position];
-        return;
-    }
-    img.src = pictures[position - 1];
-    position--;
-}
-rightBtn.addEventListener("click", moveRight);
-leftBtn.addEventListener("click", moveLeft);
+// const img = document.getElementById('carousel');
+// const rightBtn = document.getElementById('right-btn');
+// const leftBtn = document.getElementById('left-btn');
+// let pictures = ['image-slide-1.jpg', 'image-slide-2.jpg', "image-slide-3.jpg", "image-slide-4.jpg", "image-slide-5.jpg"];
+// img.src = pictures[0];
+// let position = 0;
+// const moveRight = () => {
+//     if (position >= pictures.length - 1) {
+//         position = 0
+//         img.src = pictures[position];
+//         return;
+//     }
+//     img.src = pictures[position + 1];
+//     position++;
+// }
+// const moveLeft = () => {
+//     if (position < 1) {
+//         position = pictures.length - 1;
+//         img.src = pictures[position];
+//         return;
+//     }
+//     img.src = pictures[position - 1];
+//     position--;
+// }
+// rightBtn.addEventListener("click", moveRight);
+// leftBtn.addEventListener("click", moveLeft);
